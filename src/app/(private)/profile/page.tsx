727a0be8ac72d74f8a06908bb81c96a071a12dfd
Michael <butylkin990088@mail.ru>
2026-05-13 17:15:31 +0300

    Update profile page; Add animation fadeIpUp in global.css

--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -1,26 +1,32 @@
 "use client";
 
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import {motion} from "framer-motion";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import DashboardSkeleton from "@/components/layout/DashboardSkeleton";
+
+const tabs = [
+    { id: 1, label: "Профиль", value: "overview" },
+    { id: 2, label: "Аналитика", value: "analytics" },
+    { id: 3, label: "Мои курсы", value: "courses" },
+]
 
 export default function UserDashboard() {
     return (
-        <motion.main >
+        <main>
             <div className="mt-5">
                 <h1 className="my-5 text-4xl font-bold">Dashboard</h1>
                 <Tabs defaultValue="overview">
                     <TabsList variant="line">
-                        <TabsTrigger value="overview">Профиль</TabsTrigger>
-                        <TabsTrigger value="analytics">Аналитика</TabsTrigger>
-                        <TabsTrigger value="reports">Мои курсы</TabsTrigger>
+                    {tabs.map((tab) =>
+                        <TabsTrigger key={tab.id} value={tab.value}>{tab.label}</TabsTrigger>
+                    )}
                     </TabsList>
+                    {tabs.map((tab) => (
+                        <TabsContent key={tab.id} value={tab.value} className="animate-fade-in-up shadow-sm min-h-30 p-3 rounded-md">
+                            <DashboardSkeleton />
+                        </TabsContent>
+                    ))}
                 </Tabs>
-                <div className="shadow-sm min-h-30 p-3 rounded-md">
-                    <DashboardSkeleton />
-                </div>
             </div>
-
-        </motion.main>
+        </main>
     )
 }
