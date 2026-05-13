05e3bb4e3117ef6529bd3e3f772f6e105bd059f6
Michael <butylkin990088@mail.ru>
2026-05-13 16:45:14 +0300

    create profile page

--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {motion} from "framer-motion";
+import DashboardSkeleton from "@/components/layout/DashboardSkeleton";
+
 export default function UserDashboard() {
     return (
-        <main>
-            <h1>Dashboard</h1>
-        </main>
+        <motion.main >
+            <div className="mt-5">
+                <h1 className="my-5 text-4xl font-bold">Dashboard</h1>
+                <Tabs defaultValue="overview">
+                    <TabsList variant="line">
+                        <TabsTrigger value="overview">Профиль</TabsTrigger>
+                        <TabsTrigger value="analytics">Аналитика</TabsTrigger>
+                        <TabsTrigger value="reports">Мои курсы</TabsTrigger>
+                    </TabsList>
+                </Tabs>
+                <div className="shadow-sm min-h-30 p-3 rounded-md">
+                    <DashboardSkeleton />
+                </div>
+            </div>
+
+        </motion.main>
     )
 }
